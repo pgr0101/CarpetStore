da06aaf7aa2e3e60d5911e9aef67b682505f3f0f
pgr0101 <pgr0101mm@yahoo.com>
2019-05-31 17:16:07 +0430

    finishing recommend carpet.(almost done my part)

--- models/Carpet.js
+++ models/Carpet.js
@@ -3,11 +3,6 @@ let mongoose = require('mongoose');
 
 let carpetSchema = mongoose.Schema(
     {
-        carpetId : {
-            type : Number,
-            required : true,
-            unique : true
-        },
 
         path : {
             type : String,
@@ -15,26 +10,86 @@ let carpetSchema = mongoose.Schema(
         } ,
 
         price : {
-            type : String ,
+            type : Number ,
             required : true
         } ,
 
         rate : {
             type : Number ,
+        } ,
+
+        count : {
+            type : Number
         }
     }
 );
 
 
-let Carpet = module.exports = mognoose.model('Carpet' , carpetSchema);
+let Carpet = mongoose.model('Carpet' , carpetSchema);
+module.exports = Carpet;
+
 
 module.exports.recommendCarpet = async function(cash) {
-    // TODO : returns a map of count and carpet
-    // TODO : bag algorithm
+    let carpets = await Carpet.find({}).sort({price : 1});
+    let answer = {}; // dict of => count : carpet
+    try{
+        carpets.forEach((carpet)=>{
+            let cnt = carpet.count;
+            let price = carpet.price;
+            if(cash < price){
+                throw "err";
+            }else{
+                let i = 1;
+                while(true){
+                    if(i*price > cash || i > cnt){
+                        i--;
+                        break;
+                    }
+                    i++;
+                }
+                cash -= i*price;
+                answer[carpet] = i;
+            }
+        });
+    }catch (e) {
+        console.log(e);
+    }
+
+    return answer;
 };
 
-exports.newCarpet = function(){
-  // TODO : making a new carpet and saving it
+module.exports.newCarpet = function(path , price , count , cb){
+    let carpet = new Carpet({
+       path : path ,
+       price : price ,
+       count : count ,
+       rate : 0
+    });
+    carpet.save(cb);
+};
+
+module.exports.countCarpets = async function (_id , count , cb) {
+    let carpet = await Carpet.findOne({_id : _id});
+    if(carpet.count + count < 0){
+        throw "count can't be negative";
+        return;
+    }else{
+        let carpet = await Carpet.findOne({_id : _id});
+        carpet.rate = (carpet.rate+rate)/2;
+        carpet.save(cb);
+    }
+
+};
+
+module.exports.rateCarpet = async function (_id , rate , cb) {
+    if(rate < 0){
+        throw "rate shouldn't be negative";
+        return;
+    }else{
+        let carpet = await Carpet.findOne({_id : _id});
+        carpet.rate = (carpet.rate+rate)/2;
+        carpet.save(cb);
+    }
 };
 
 
--- routes/users.js
+++ routes/users.js
@@ -2,121 +2,167 @@ var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
 var Map = require('../models/Map');
+var Carpet = require('../models/Carpet');
 
 /**
  * router middleware for jwt auth
  * */
 
 router.use(function (req, res, next) {
-  let token = req.headers['pgr-token'];
-  if (token) {
-    jwt.verify(token, "pgr0101secret", (err, decoded) => {
-      if (err) {
-        res.status(403).json({
-          msg: 'Token is not valid'
+    let token = req.headers['pgr-token'];
+    if (token) {
+        jwt.verify(token, "pgr0101secret", (err, decoded) => {
+            if (err) {
+                res.status(403).json({
+                    msg: 'Token is not valid'
+                });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
         });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    res.status(403).json({
-      msg: 'Access denied'
-    });
-  }
-}); 
+    } else {
+        res.status(403).json({
+            msg: 'Access denied'
+        });
+    }
+});
 
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  // TODO : profile handling
-  //  returning all the carpets s/he added
-  res.json({
-    msg : 'all the carpets'
-  });
+router.get('/', function (req, res) {
+    // TODO : profile handling
+    //  returning all the carpets s/he added
+    res.json({
+        msg: 'all the carpets'
+    });
+});
+
+router.post('/newcarpet', function (req, res) {
+    Carpet.newCarpet((!req.body.path ? null : req.body.path)
+        , req.body.price, req.body.count, (err, carpet) => {
+
+            if (err) {
+                res.status(406).json({
+                    msg: "something went wrong",
+                    err: err
+                });
+            } else {
+                res.status(406).json({
+                    msg: "something went wrong",
+                    carpet: carpet
+                });
+            }
+        });
+});
+
+router.post('/countcarpet', async function (req, res) {
+    try {
+        await Carpet.countCarpets(req.body._id, req.body.count,
+            function (err, carpet) {
+                if (err) {
+                    res.status(406).json({
+                        msg: "something went wrong",
+                        err: err
+                    });
+                } else {
+                    res.status(406).json({
+                        msg: "something went wrong",
+                        carpet: carpet
+                    });
+                }
+            });
+    } catch (e) {
+        res.status(406).json({
+            msg: "something went wrong",
+            err: e
+        });
+    }
+});
+
+router.post('/ratecarpet', async function (req, res) {
+    try {
+        await Carpet.rateCarpet(req.body._id, req.body.rate,
+            function (err, carpet) {
+                if (err) {
+                    res.status(406).json({
+                        msg: "something went wrong",
+                        err: err
+                    });
+                } else {
+                    res.status(406).json({
+                        msg: "something went wrong",
+                        carpet: carpet
+                    });
+                }
+            });
+    } catch (e) {
+        res.status(406).json({
+            msg: "something went wrong",
+            err: e
+        });
+    }
+});
+
+router.get('/mapsme/:location', async function (req, res) {
+    let location = parseInt(req.params.location);
+    let router = await Map.mapsMe(location);
+    res.json({
+        msg: "routing the user to the nearest Store",
+        router: router
+    });
+});
+
+router.get('/bywithmoney/:money', async function (req, res) {
+    let answer = await Carpet.recommendCarpet(parseInt(req.params.money));
+    res.json({
+        msg: "recommending carpets with limited money",
+        answer : answer
+    });
 });
 
 
-router.post('/newCarpet' , function (req, res) {
-  /**
-   * TODO : adding new carpet plans
-   *  handling normal things like price and color
-   * */
-  res.json({
-    msg : "adding new carpet"
-  });
+router.post('/changecarpet', function (req, res) {
+    /**
+     * TODO : changing carpet color
+     *  this is a filter
+     * */
+    res.json({
+        msg: "changing carpet"
+    });
 });
 
-
-router.post('/changeCarpet' , function (req, res) {
-  /**
-   * TODO : changing carpet color
-   *  this is a filter
-   * */
-  res.json({
-    msg : "changing carpet"
-  });
+router.post('/findbyplan', function (req, res) {
+    /**
+     * TODO : finding carpets by
+     *  plan likeness and maybe color similarity
+     * */
+    res.json({
+        msg: "finding carpet by plan likeness"
+    });
 });
-
-
-router.post('/findbyplan' , function (req, res) {
-  /**
-   * TODO : finding carpets by
-   *  plan likeness and maybe color similarity
-   * */
-  res.json({
-    msg : "finding carpet by plan likeness"
-  });
-});
-
-
-router.get('/bywithmoney/:money', function (req, res) {
-  /**
-   * TODO : finding the more carpets
-   *  user can by with the max money that has
-   *  handling by Carpet.recommendCarpet
-   * */
-  res.json({
-    msg : "recommending carpets with limited money"
-  });
-});
-
-
-router.get('/mapsme/:location' ,async function (req, res) {
-  let location = parseInt(req.params.location);
-  let router = await Map.mapsMe(location);
-  res.json({
-    msg : "routing the user to the nearest Store",
-    router : router
-  });
-});
-
-
-
-
 
 
 /** for faze 2 : */
-router.post('/addtocart/:id' , function (req, res) {
-  /**
-   * TODO : adding a product to cart
-   *  and calculating the hole price
-   * */
+router.post('/addtocart/:id', function (req, res) {
+    /**
+     * TODO : adding a product to cart
+     *  and calculating the hole price
+     * */
 });
 
-router.get('/buy' ,function (req, res) {
-  /**
-   * TODO : returning cart to the user
-   *  and also payment options
-   * */
+router.get('/buy', function (req, res) {
+    /**
+     * TODO : returning cart to the user
+     *  and also payment options
+     * */
 });
 
-router.post('/deletefromcart/:id' , function (req, res) {
-  /**
-   * TODO : deleting a product from the cart
-   *  of the user before payment
-   * */
+router.post('/deletefromcart/:id', function (req, res) {
+    /**
+     * TODO : deleting a product from the cart
+     *  of the user before payment
+     * */
 });
 
 module.exports = router;
